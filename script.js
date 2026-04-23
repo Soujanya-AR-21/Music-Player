381ef4e7a8741876e96239984749c25771f691bf
Soujanya A R <soujanyaar21@gmail.com>
2026-04-23 20:59:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const audioPlayer = document.getElementById('audioPlayer');
 const playPauseButton = document.getElementById('playPauseButton');
 const nextButton = document.getElementById('nextButton');
 const prevButton = document.getElementById('prevButton');
+const trackNameElement = document.getElementById('trackName');
+const imageElement = document.getElementById('image');
+const volumeControl = document.getElementById('volumeControl');
